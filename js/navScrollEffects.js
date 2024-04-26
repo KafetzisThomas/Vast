21dc940a8d0c3c40a39de49fb3da1aa989d5f03f
KafetzisThomas <tomkafetzis06@gmail.com>
2024-04-27 00:00:59 +0300

    Add comments to js files

--- js/navScrollEffects.js
+++ js/navScrollEffects.js
@@ -1,16 +1,27 @@
 // Update active class on navbar links when scrolling
+
+// Listen for the scroll event on the window
 window.addEventListener('scroll', function () {
+    // Get all the sections on the page
     let sections = document.querySelectorAll('section');
+    // Get all the navigation links
     let navLinks = document.querySelectorAll('.nav-link');
+    // Determine the scroll position where the active effect should trigger
     let scrollPosition = window.innerHeight / 2; // Adjust this value to change when the underline effect starts
 
+    // Loop through each section
     sections.forEach((section, index) => {
+        // Get the top position of the current section
         let top = section.offsetTop;
+        // Get the height of the current section
         let height = section.offsetHeight;
+        // Check if the window's scroll position is within the current section
         if (window.scrollY + scrollPosition >= top && window.scrollY < top + height) {
+            // Remove the 'active' class from all navigation links
             navLinks.forEach(link => {
                 link.classList.remove('active');
             });
+            // Add the 'active' class to the corresponding navigation link
             navLinks[index].classList.add('active');
         }
     });
--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,22 @@
 // Smooth scroll to section when clicking on navbar links & buttons
 document.querySelectorAll('.scroll-btn').forEach(btn => {
+    // Add event listener to each button
     btn.addEventListener('click', function (e) {
-        e.preventDefault();
+        e.preventDefault(); // Prevent default link behavior
 
+        // Extract the id of the target section from the button's data attribute
         const targetId = this.getAttribute('data-target').substring(1);
+
+        // Get the target section element by its id
         const targetSection = document.getElementById(targetId);
+
+        // Define the height of the navbar (adjust if necessary)
         const navbarHeight = 100;
+
+        // Calculate the offset from the top to scroll to, considering navbar height
         const offsetTop = targetSection.offsetTop - navbarHeight;
 
+        // Scroll to the target section with smooth behavior
         window.scrollTo({
             top: offsetTop,
             behavior: 'smooth'
