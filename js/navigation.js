407b1b0f865ff0beb0e8d72ce0a2d4af0d91157a
KafetzisThomas <tomkafetzis06@gmail.com>
2024-04-26 23:55:16 +0300

    Implement pre-loader spinner functionality

--- js/navigation.js
+++ js/navigation.js
@@ -14,21 +14,3 @@ document.querySelectorAll('.scroll-btn').forEach(btn => {
         });
     });
 });
-
-// Update active class on navbar links when scrolling
-window.addEventListener('scroll', function () {
-    let sections = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('.nav-link');
-    let scrollPosition = window.innerHeight / 2; // Adjust this value to change when the underline effect starts
-
-    sections.forEach((section, index) => {
-        let top = section.offsetTop;
-        let height = section.offsetHeight;
-        if (window.scrollY + scrollPosition >= top && window.scrollY < top + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
-            navLinks[index].classList.add('active');
-        }
-    });
-});
